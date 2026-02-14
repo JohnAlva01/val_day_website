a7bf95da5542bd598916b820123e9851fa3e2c5b
John Alvaro <john.d.alvaro@gmail.com>
2026-02-14 04:36:14 -0700

    d

--- js/moments-data.js
+++ js/moments-data.js
@@ -1,7 +1,7 @@
 export const momentsData = [
   {
     title: "Japan Vacation",
-    video: "../assets/Japan.mp4",
+    video: "./assets/Japan.mp4",
     text:
 `This was my first international trip with a partner, and it was the craziest unforgettable experience. The fact that I got to go to JAPAN with you makes it even more crazy to me
 
@@ -9,7 +9,7 @@ All of our eats, talks, and adventures made me realize that life was more than j
   },
   {
     title: "Sylvan Lake Vibes",
-    video: "../assets/Sylvan.MP4",
+    video: "./assets/Sylvan.MP4",
     text:
 `Sylvan lake was such an impromptu and fun roadtrip that made me really relax. I remember swimming and eating underneath some perfect weather. This was also the time that I felt like I got to know you and your friends pretty well (as well as get brownie points from them for having a good first impression). For me, it again healed another part of my inner child because we just straight up went and left Edmonton with nobody stopping us. The sunset was absolutely beautiful after the food. Both Jesslen and Ate Yna both vibing with us and I felt kinda gassed that they were comfortable with me, like yes I'm her boyfriend but I'm chill too. 
 
@@ -17,7 +17,7 @@ The water was clear, the sun was up, the pork belly was bussing, and the vibes w
   },
   {
     title: "Choreographing a Duet",
-    video: "../assets/Dance.mp4",
+    video: "./assets/Dance.mp4",
     text:
 `I remember you requesting to make this dance because before you left for Korea you wanted to create something with me. I do like choreographing but the last time I did a duet with anyone was like 12 years ago, maybe more which happened cus I was in a dance school at the time. I was a bit skeptical because I wasn't sure how we were going to interact when making it but I wasn't worried the moment we started choreographing. Every moment I enjoyed and the final product was probably the biggest hit in the OSD showcase (not to brag or anything hehe). Creating something with you gave me so much insight on who you are as a person and how we could work together as a team. I trusted you as much as you trusted me in the process and in the end we created something sick af.
 
